6ec8cd3d4550d06b89ae15f98a4017dd573a2e14
JulienCouderc <juliencouderc36@gmail.com>
2023-06-02 16:34:38 +0200

    UPDATE

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String texte = "exemple de texte a classer";
-        List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        System.out.println(Exercice1.solution(texte,ordre));
+        String texte = "exemple de texte exemplaire exemples a classer wario nigga 672 xylophone mignon ae at";
+        List<Character> ordre = List.of('c', 't', 'e', '6', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
+        System.out.println(ExerciceSimplicitePire.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
---
+++ src/test/java/ExerciceSimplicitePireTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+import exercice.ExerciceSimplicitePire;
+import java.util.*;
+
+public class ExerciceSimplicitePireTest {
+    @Test
+    public void testSolution() {
+        // Chaine simple avec ordre complet
+        assertEquals(List.of("666","the", "the", "number", "of", "beast"), ExerciceSimplicitePire.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
+        // Chaine à 1 mot
+        assertEquals("Erreur de chaine à 1 mot", List.of("OK"), ExerciceSimplicitePire.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
+        // Chaine vide
+        assertEquals(List.of(""), ExerciceSimplicitePire.solution("",List.of('6', 't', 'n', 'o', 'b')));
+        // Chaine donnée en exemple
+        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"), ExerciceSimplicitePire.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+        // Ajoutez vos test ici...
+    }
+}
+
