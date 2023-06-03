28f3778f9fc40230510886254a2044324d8ede21
JulienCouderc <juliencouderc36@gmail.com>
2023-06-03 12:16:57 +0200

    commit

---
+++ src/main/java/exercice/ExerciceSimpliciteMeilleur.java
@@ -0,0 +1,99 @@
+package exercice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciceSimpliciteMeilleur {
+    public static List<String> solution(String str, List<Character> ordre) {
+        String[] mots = str.split("[^a-zA-Z0-9]");
+
+        List<String> motsClasses = new ArrayList<>();
+        List<String> motsAvecLettreInconnue = new ArrayList<>();
+
+        for (String mot : mots) {
+            if (mot.length() > 0) {
+                if (commenceParLettre(mot, ordre)) {
+                    boolean motInseré = false;
+                    for (int i = 0; i < motsClasses.size(); i++) {
+                        String motClasse = motsClasses.get(i);
+                        if (compareMots(mot, motClasse, ordre) < 0) {
+                            motsClasses.add(i, mot);
+                            motInseré = true;
+                            break;
+                        }
+                    }
+                    if (!motInseré) {
+                        motsClasses.add(mot);
+                    }
+                } else {
+                    motsAvecLettreInconnue.add(mot);
+                }
+            }
+        }
+
+        motsClasses.addAll(motsAvecLettreInconnue);
+
+        return motsClasses;
+    }
+
+    private static boolean commenceParLettre(String mot, List<Character> ordre) {
+        if (ordre.size()==0) {
+            return true;
+        }
+        char premiereLettre = mot.charAt(0);
+        return ordre.contains(premiereLettre);
+    }
+
+    private static int compareMots(String mot1, String mot2, List<Character> ordre) {
+
+        int longueurMot1 = mot1.length();
+        int longueurMot2 = mot2.length();
+        int longueurMinimale;
+
+        if(longueurMot1>longueurMot2){
+            longueurMinimale = longueurMot2;
+        }else{
+            longueurMinimale = longueurMot1;
+        }
+        for (int i = 0; i < longueurMinimale; i++) {
+            char char1 = mot1.charAt(i);
+            char char2 = mot2.charAt(i);
+    
+            if (char1 != char2) {
+                int index1 = -1;
+                int index2 = -1;
+                
+                for (int j = 0; j < ordre.size(); j++) {
+                    if (ordre.get(j) == char1) {
+                        index1 = j;
+                        break;
+                    }
+                }
+                
+                for (int j = 0; j < ordre.size(); j++) {
+                    if (ordre.get(j) == char2) {
+                        index2 = j;
+                        break;
+                    }
+                }
+                
+                if (index1 < index2) {
+                    return -1; 
+                } else {
+                    return 1; 
+                }
+            }
+        }
+
+        if (longueurMot1 < longueurMot2) {
+            return -1;
+        } else if (longueurMot1 > longueurMot2) {
+            return 1;   
+        } else {
+            return 0; 
+        }
+        
+    }
+}
+
+
--- src/test/java/ExerciceSimpliciteMeilleurTest.java
+++ src/test/java/ExerciceSimpliciteMeilleurTest.java
@@ -2,20 +2,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
-import exercice.ExerciceSimplicitéMeilleur;
+import exercice.ExerciceSimpliciteMeilleur;
 import java.util.*;
 
 public class ExerciceSimpliciteMeilleurTest {
     @Test
     public void testSolution() {
         // Chaine simple avec ordre complet
-        assertEquals(List.of("666","the", "the", "number", "of", "beast"), ExerciceSimplicitéMeilleur.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
+        assertEquals(List.of("666","the", "the", "number", "of", "beast"), ExerciceSimpliciteMeilleur.solution("666, the number of the beast",List.of('6', 't', 'n', 'o', 'b')));
         // Chaine à 1 mot
-        assertEquals("Erreur de chaine à 1 mot", List.of("OK"), ExerciceSimplicitéMeilleur.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
+        assertEquals("Erreur de chaine à 1 mot", List.of("OK"), ExerciceSimpliciteMeilleur.solution("OK",List.of('a', 'b', 'c', 'd', 'e')));
         // Chaine vide
-        assertEquals(List.of(""), ExerciceSimplicitéMeilleur.solution("",List.of('6', 't', 'n', 'o', 'b')));
+        assertEquals(List.of(), ExerciceSimpliciteMeilleur.solution("",List.of('6', 't', 'n', 'o', 'b')));
         // Chaine donnée en exemple
-        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"), ExerciceSimplicitéMeilleur.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
+        assertEquals(List.of("fait", "Il", "aujourd", "aout", "beau", "hui", "comme", "en"), ExerciceSimpliciteMeilleur.solution("Il fait beau aujourd'hui comme en aout",List.of('f', 'I', 'z', 'u', 'k', 'a', 'b', 'o')));
         // Ajoutez vos test ici...
     }
 }
